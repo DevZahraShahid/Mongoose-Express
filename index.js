ceaf5c50e762544db5beb6d2e091803d635ce142
Zahra Shahid <zahrashahid@gosaas.io>
2022-08-16 17:29:29 +0500

    validators added

--- index.js
+++ index.js
@@ -7,11 +7,44 @@ mongoose
 
 // 2- Create Schema for the collection
 const courseSchema = mongoose.Schema({
-  name: String,
+  name: { type: String, required: true },
   author: String,
-  tags: [String],
+  // tags: [String],
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
+
+  //Built-in Validator
+  // category: {
+  //   type: String,
+  //   required: true,
+  //   enum: ["School", "College", "phD"], // must include only these
+  // },
+
+  //Custom Validator
+  tags: {
+    type: Array,
+    validate: {
+      validator: function (v) {
+        return v && v.length > 0;
+      },
+      message: "A course should have atleast one tag",
+    },
+  },
+
+  //ASYNC Validator
+  // tags: {
+  //   type: Array,
+  //   validate: {
+  //     isAsync: true,
+  //     validator: function (v, callback) {
+  //       setTimeout(() => {
+  //         const result = v && v.length > 0;
+  //         callback(result);
+  //       }, 2000);
+  //     },
+  //     message: "A course should have atleast one tag",
+  //   },
+  // },
 });
 
 //Class, Object
@@ -23,14 +56,21 @@ const Course = mongoose.model("Course", courseSchema);
 // 4- CRreate objects using ASYNC/AWAIT - as .save(), .find() returns a promise
 async function createCourse() {
   const course = new Course({
-    name: "Cybersecurity Course",
+    name: "Angular Course",
     author: "Mosh",
-    tags: ["Cybersecurity", "Blockchain"],
+    tags: ["full stack"],
     isPublished: true,
   });
 
-  const result = await course.save(); //returns an ID
-  console.log(result);
+  try {
+    const result = await course.save(); //returns an ID
+    console.log(result);
+  } catch (exc) {
+    // To list down all the errors
+    for (field in exc.errors) {
+      console.log(exc.errors[field].message);
+    }
+  }
 }
 // createCourse();
 
@@ -45,7 +85,7 @@ async function getFilteredCourses() {
   const pageNumber = 2;
   const pageSize = 20;
 
-  const courses = await Course.find({ author: "Mosh", isPublished: true })
+  const courses = await Course.find({ author: "Zahra", isPublished: true })
     // .find({ price: { $in: [10, 30, 50] } })  // comparison operators
     // .or([{ author: "Mosh", isPublished: true }])  // logical operators
     // .find({ author: /^Mosh/ }) // starts with
@@ -68,8 +108,8 @@ async function getFilteredCourses() {
 async function updateCourse(id) {
   const course = await Course.findById(id);
   if (!course) return;
-  course.isPublished = false;
-  course.author = "New Author";
+  course.isPublished = true;
+  course.author = "Anonymous";
   const result = await course.save();
   console.log(result);
 }
@@ -88,11 +128,11 @@ async function updateCourseDirectly(id) {
   );
   console.log(result);
 }
-// updateCourseDirectly("62f5f004a9208a87af34f076");
+// updateCourseDirectly("62f5f0ff76902241b6f7344d");
 
 // Remove
 async function removeCourse(id) {
   const result = await Course.deleteOne({ _id: id });
   console.log(result);
 }
-// removeCourse("62f5f004a9208a87af34f076");
+// removeCourse("62fa2ff4e23e07d58f26292b");
